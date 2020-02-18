bfbf21992a055510c50703c1e03eea96e4bbfccd
Vincent Servio <vincentservio@gmail.com>
2020-02-18 18:35:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,8 @@
 # Make your shoe class here!
 
 class Shoe
-  attr_accessor :color, :size, :material
-  attr_reader :brand, :condition 
+  attr_accessor :color, :size, :material, :condition 
+  attr_reader :brand
   
   def initialize(brand)
     @brand = brand
