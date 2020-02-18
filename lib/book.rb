eeba807d2b571b5af2e9ced015ea149326ed8f60
Vincent Servio <vincentservio@gmail.com>
2020-02-18 18:11:22 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,5 +12,13 @@ end
 def author
   @author
 end 
+ 
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
 end 
 # Book.new("And Then There Were None")
